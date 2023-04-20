f47e7a1fdd0ba9c9d56a9ed238af970f23dcfb53
Yassine Mathlouthi <Yassinmathlouthi5@gmail.com>
2023-04-20 14:07:39 +0100

    third

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,14 @@ export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    constructor(private renderer: Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element: ElementRef, public location: Location) {}
+
     ngOnInit() {
-        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        var navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
         this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
             if (window.outerWidth > 991) {
                 window.document.children[0].scrollTop = 0;
-            }else{
+            } else {
                 window.document.activeElement.scrollTop = 0;
             }
             this.navbar.sidebarClose();
@@ -28,41 +29,29 @@ export class AppComponent implements OnInit {
         this.renderer.listen('window', 'scroll', (event) => {
             const number = window.scrollY;
             if (number > 150 || window.pageYOffset > 150) {
-                // add logic
                 navbar.classList.remove('navbar-transparent');
             } else {
-                // remove logic
                 navbar.classList.add('navbar-transparent');
             }
         });
         var ua = window.navigator.userAgent;
         var trident = ua.indexOf('Trident/');
         if (trident > 0) {
-            // IE 11 => return version number
             var rv = ua.indexOf('rv:');
             var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
         }
         if (version) {
             var body = document.getElementsByTagName('body')[0];
             body.classList.add('ie-background');
-
         }
-
     }
-    removeNavbarAndFooter() {
-        const titlee = this.location.prepareExternalUrl(this.location.path()).slice(1);
-        if (titlee === 'signup') {
-          return false;
+
+    removeNavbarAndFooter(): boolean {
+        const titlee = this.location.prepareExternalUrl(this.location.path());
+        if (titlee.includes('/signup')) {
+            return false;
         } else {
-          // Hide the navbar
-          const navbar = document.querySelector('.navbar') as HTMLElement;
-          if (navbar) {
-              navbar.style.display = 'none';
-          }
-          // Hide the footer
-          return true;
+            return true;
         }
-      }
-    
-      
+    }
 }
