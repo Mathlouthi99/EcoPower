33987964f6ad44a197bd96b8757d17ef91a2ac59
Yassine Mathlouthi <Yassinmathlouthi5@gmail.com>
2023-04-20 16:28:09 +0100

    thirdd

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,11 @@ import { ComponentsComponent } from './components/components.component';
 import { ProfileComponent } from './examples/profile/profile.component';
 import { SignupComponent } from './examples/signup/signup.component';
 import { LandingComponent } from './examples/landing/landing.component';
+import { AddCategoryComponent } from './examples/admin/add-category/add-category.component';
+import { ListCategoryComponent } from './examples/admin/list-category/list-category.component';
+import { AddProductComponent } from './examples/admin/add-product/add-product.component';
+//import { ListProductComponent } from './examples/admin/list-product/list-product.component';
+import { DashboardComponent } from './examples/admin/dashboard/dashboard.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
 const routes: Routes =[
@@ -15,7 +20,13 @@ const routes: Routes =[
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
     { path: 'landing',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent }
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    {path: 'add', component: AddProductComponent},
+    {path: 'listProduct', component: ListProductComponent },
+    {path: 'admin', component: DashboardComponent},
+    {path: 'addCategory', component: AddCategoryComponent},
+    {path: 'listCategory', component: ListCategoryComponent},
+
 ];
 
 @NgModule({
